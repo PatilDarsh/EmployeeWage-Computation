7af5aeffd35dfa4f27a61bec6ed0b21801d33edf
PatilDarsh <darshpatil072@gmail.com>
2024-02-15 16:59:01 +0530

    [MODIFIED]:Darshan - UC-1 Completed for Employee Attendance

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -1,10 +1,26 @@
 package com.infogalaxy.employeewagecomp;
+import java.util.Random ;
 
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int attendance = 0;
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		Random random = new Random() ;
+		attendance = random.nextInt(9)%2 ;
+		
+		if (attendance == 0)
+		{
+			System.out.println("Employee Is Absent");
+		}
+		else
+		{
+			System.out.println("Employee Is Present");
+		}
 
 	}
 
